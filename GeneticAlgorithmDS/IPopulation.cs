d4cefe34ac9534706bae50c6473231a39f605a8d
Donovan Martodikromo <donovan.dcm@gmail.com>
2017-04-17 18:47:30 +0200

    Start1

---
+++ GeneticAlgorithmDS/IPopulation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithmDS
+{
+    class IPopulation
+    {
+        //Hi
+        //Test
+        //Nee
+    }
+}
